fb0828d38e97038434e84c426f2a889f60a9ddcf
Balveer <balveer.iyc@gmail.com>
2025-10-16 18:57:19 +0530

    Initial chnages

---
+++ ArohiWebsite.Web/Components/Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿namespace ArohiWebsite.Web.Components.Models
+{
+    public class InvoiceModel
+    {
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalAmount => Quantity * Price;
+        public string PaymentStatus { get; set; }
+    }
+
+}
